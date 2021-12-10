779b8863c6284e22953f06690d946e21d95e8d4d
Aubrey Carlon <aubreycarlon@Pauls-MacBook-Pro.local>
2021-12-09 21:16:06 -0800

    encrypt messages with python!

---
+++ caesar-cipher.py
@@ -0,0 +1,22 @@
+#Caesar Cypher
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, to 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ''
+    if cipher_direction == 'decode':
+            shift_amount *= -1
+    for letter in start_text:
+        if letter == ' ':
+            end_text += ' '
+        else:
+            position = alphabet.index(letter)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+    print(f'The {cipher_direction}d text is: \n\n{end_text}')    
+
+caesar(start_text = text, shift_amount=shift, cipher_direction= direction)
